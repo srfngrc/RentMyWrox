438a51e737d0cac32511f878c0912064b091344e
srfngrc <srfngrc@gmail.com>
2019-08-08 18:55:35 -0400

    I added ALL THE TODAYS EXAMPLE at the start of the class studentMODEL-indexVIEW-studentCONTROLLER Aug8th 6h55pm

---
+++ RentMyWrox/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentMyWrox.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentModel myStudent = new StudentModel();
+            myStudent.firstName = "Serafin";
+            myStudent.lastName = "Garcia";
+            myStudent.studentId = "IE8438490303";
+            return View(myStudent);
+        }
+    }
+}
